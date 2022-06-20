31a9f29effd4776e7979d01b72ed75c593381d9e
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-06-21 08:34:59 +0900

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-function App() {
+import QuestionCard from './components/QuestionCard';
+
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        React TypeScript Quiz
-      </header>
+    <div className='App'>
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
-}
+};
 
 export default App;
