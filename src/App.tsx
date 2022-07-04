309e5ee247e8a4f1d9658a9fb971a2c9c2322e47
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-07-04 22:41:15 +0900

    remove null syntax

--- src/App.tsx
+++ src/App.tsx
@@ -109,7 +109,7 @@ const App: React.FC = () => {
           ) : (
             <p className='score'>Score: {score}</p>
           )}
-          {loading ? <p>Loading Questions ...</p> : null}
+          {loading && <p>Loading Questions ...</p>}
           {!loading && !gameOver && (
             <QuestionCard
               questionNumber={number + 1}
@@ -123,11 +123,11 @@ const App: React.FC = () => {
           {!gameOver &&
           !loading &&
           userAnswers.length === number + 1 &&
-          number !== TOTAL_QUESTIONS - 1 ? (
+          number !== TOTAL_QUESTIONS - 1 && (
             <button className='next btn' onClick={nextQuestion}>
               Next Question
             </button>
-          ) : null}
+          )}
           {!loading && userAnswers.length === TOTAL_QUESTIONS && (
             <div className='btnBoxWrap'>
               <div className='btnBox'>
